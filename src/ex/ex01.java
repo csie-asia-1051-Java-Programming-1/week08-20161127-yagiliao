209ba2b5c26156222c98228574dfe90e01f37808
yagiliao <105021039@live.asia.edu.tw>
2016-11-22 03:49:08 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -2,14 +2,24 @@
 /*
  * Topic: 請寫一個函式 (不是遞迴) 進行十進制數字轉換成二進制數字，例如十進制的 13 會輸出二進制的 1101
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021039 廖野吉
  */
-
+import java.util.Scanner;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int val=scn.nextInt();
+		System.out.println(fun(val));
+	}
+	public static String fun(int val){
+		String a="";
+		while(val>0){
+			a=Integer.toString(val%2)+a;
+			val/=2;
+		}
+		return a;
 	}
 
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,37 @@
 /*
  * Topic: 將前一題加強成可以做10以下的各種數字系統轉換
  * Date: 2016/11/21
- * Author: 1050210XX 周永振老師
+ * Author: 105021039 廖野吉
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		System.out.println("請輸入數字");
+		int val=scn.nextInt();
+		System.out.println("請輸入這是幾進位");
+		double way=scn.nextInt();
+		System.out.println("請輸入要轉換為幾進位");
+		int last=scn.nextInt();
+		System.out.println(fun(val,way,last));
 	}
-
+	public static String fun(int val,double way,int last){
+		double a,sum=0;
+		String b="";
+		b=Integer.toString(val);
+		double c=b.length()-1;
+		for(int i=0;i<b.length();i++){
+			sum=sum+(b.charAt(i)-48)*(Math.pow(way,c));
+			c--;
+		}
+		b="";
+		int tal=(int)sum;
+		while(tal>0){
+			b=Integer.toString(tal%last)+b;
+			tal=tal/last;
+		}
+		return b;
+	}
 }
